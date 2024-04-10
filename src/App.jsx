941686a0219390f98bf875b0e7275e712bfe0827
Akash9698 <senthilsenthil98832@gmail.com>
2024-04-10 17:38:34 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Quiz from './components/Quiz'
+import Result from './components/Result'
+import Home from './components/Home'
+
+function App() {
+
+  return (
+    <>
+      <Quiz/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './App.css'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div id='body'>
+        <div id='content1'>
+        <h1 id='heading'>Quiz App</h1>
+        <button id='playBtn'>Play</button>
+        </div>
+      </div>
+    )
+  }
+}
